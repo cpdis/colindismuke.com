11336a2ac725a931fdb30d0dbef8adc0297a44f1
Colin Dismuke <colin.dismuke@gmail.com>
2021-09-02 23:10:38 -0500

    :lipstick: Changed width on resources page

--- pages/resources.js
+++ pages/resources.js
@@ -7,7 +7,7 @@ export default function Resources() {
       title="Resources – Colin Dismuke"
       description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
     >
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+      <div className="w-full flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Resources
         </h1>
